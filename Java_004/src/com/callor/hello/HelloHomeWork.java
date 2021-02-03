38403408de35810b3153407248559c09e6bf84a6
KIMSOLBIN <lgko1122@naver.com>
2021-02-03 15:15:43 +0900

    변수이야기

--- Java_004/src/com/callor/hello/HelloHomeWork.java
+++ Java_004/src/com/callor/hello/HelloHomeWork.java
@@ -5,12 +5,7 @@ package com.callor.hello;
 public class HelloHomeWork {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-			System.out.println("반갑습니다");
-=======
-		System.out.println("반갑습니다");
 
->>>>>>> 01c84339ea0025cbf028ba051ad0928b728efe3e
 	}
 
 }
---
+++ Java_005/src/com/callor/var/VarFloat.java
@@ -0,0 +1,21 @@
+package com.callor.var;
+
+/*
+ * 실수를 표현하기 2
+ * float
+ */
+public class VarFloat {
+
+	public static void main(String[] args) {
+		/*
+		 * float(단정도실수형)변수 num1을 선언하고
+		 * float형 값 50.0을 저장하라
+		 * 이때 숫자값에는 F(f)을 붙여야 한다
+		 */
+		float num1 = 50.0F;
+		float num2 = 50.111111111111111111F;
+		System.out.println(num1);
+		System.out.println(num2);
+	}
+
+}
