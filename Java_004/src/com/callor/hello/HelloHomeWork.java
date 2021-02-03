07120fcfe2532873c78771f1a5dda8b887676f0c
KIMSOLBIN <lgko1122@naver.com>
2021-02-03 10:34:12 +0900

    집에서 연습

---
+++ Java_004/src/com/callor/hello/HelloHomeWork.java
@@ -0,0 +1,12 @@
+package com.callor.hello;
+/*
+ *  이 프로젝트는 집에서 공부를 위해 추가한 것
+ */
+public class HelloHomeWork {
+
+	public static void main(String[] args) {
+		System.out.println("반갑습니다");
+
+	}
+
+}
