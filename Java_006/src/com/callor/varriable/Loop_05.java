2a16d7cf31aa3017121eb6f5b40d3c41cbfa1861
KIMSOLBIN <lgko1122@naver.com>
2021-02-04 16:33:46 +0900

    반복문

--- Java_006/src/com/callor/varriable/Loop_05.java
+++ Java_006/src/com/callor/varriable/Loop_05.java
@@ -5,7 +5,24 @@ public class Loop_05 {
 	public static void main(String[] args) {
 		
 		int intDan = 7;
+		for(int num = 1; num <10 ; num += 1) {
+			
+		}
 
+		for(int num = 1; num < 10 ; num += 1) {
+			
+		}
+		
+		for(int num = 20 ; num < 40 ; num += 1 ) {
+			System.out.println("안녕");
+		}
+		//  시작하는값    비교하는값  증가하는값
+		for(int num = 12 ; num <= 20 ; num += 1) {
+			System.out.println("반가워");
+		}
+		for(int num = 12 ; num <= 20 ; num += 2) {
+			System.out.println(num);
+		}
 	}
 
 }
