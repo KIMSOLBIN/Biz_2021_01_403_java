2db411c1c6f2b5beac5fb6400e7c6124432d49e2
KIMSOLBIN <lgko1122@naver.com>
2021-02-24 09:40:02 +0900

    prime

--- Java_015/src/com/callor/applications/HomeWork_01.java
+++ Java_015/src/com/callor/applications/HomeWork_01.java
@@ -11,21 +11,25 @@ public class HomeWork_01 {
 		Random rnd = new Random();
 		int[] num = new int[100];
 		int i = 0;
-		for (i = 0; i < num.length; i++) {
-			num[i] = rnd.nextInt(100) + 1;
+		for (i = 0; i < num.length; i++) { // int[] num에 들어간 100개을 배열하면 0~99이므로
+			num[i] = rnd.nextInt(100) + 1; // Random 코드를 이용하여 rnd.nextInt에 (100)+ 1을 더하여 1 ~ 100이라는 숫자를 만들어준다.
 		}
 		PrimeServiceV2 ccV2 = new PrimeServiceV2();
 
 		int sum = 0;
 		for (i = 0; i < num.length; i++) {
-			int result = ccV2.prime(num[i]);
+			int result = ccV2.prime(num[i]); // 위 Random을 이용한 값을 PrimeserviceV2파일에 넘겨 대입한다
+											// PrimeservieV2에는 return코드가 사용 되어있으므로 "int result"를 이용하여 계산결과값을 불러온다.
 
-			if (result > 0) {
-				System.out.println(result);
-				sum += result;
+			//소수 리스트 출력
+			if (result > 0) { // result에 들어간 PrimeserviceV2의 계산결과로 소수이면 -1 또는 소수가 아니면 0 이라는 값이 result에 저장
+				System.out.println(result); // if문에 들어간 조건이 참이면 출력문에 result를 넣게 되면 소수인 숫자들이 출력됨
+				sum += result;// 리스트에 나온 소수들의 합을 구하기 위해 계속 더해준다는 "+="을 하여 소수들을 더해준다.
 			}
 		}
 
-		System.out.println("소수의 합 :" + sum);
+		System.out.println("소수의 합 :" + sum); // 소수의 합의 결과는 한번만 나와야 하기 때문에
+												 // for(반복문){} 밖에 출력해야한다.
+												 //  만약에 for(반복문)에 {}안에 출력한다면 값을 무한 반복출력하게 된다.
 	}
 }
