5a5132f6f4b7350433140e32b2a4f9ce95faadde
KIMSOLBIN <lgko1122@naver.com>
2021-02-24 09:02:08 +0900

    prime

---
+++ Java_015/src/com/callor/applications/HomeWork_01.java
@@ -0,0 +1,34 @@
+package com.callor.applications;
+
+import java.util.Random;
+
+import com.callor.applications.service.PrimeServiceV2;
+
+public class HomeWork_01 {
+
+	public static void main(String[] args) {
+
+		Random rnd = new Random();
+		int[] num = new int[10];
+		int i = 0;
+		for (i = 0; i < num.length; i++) {
+			num[i] = rnd.nextInt(100) + 1;
+		}
+		PrimeServiceV2 ccV2 = new PrimeServiceV2();
+
+		int sum = 0;
+		for (i = 0; i < num.length; i++) {
+			int result = ccV2.prime(num[i]);
+
+			if (result < 0) {
+
+			} else {
+				System.out.println(result);
+				sum += result;
+			}
+
+		}
+
+		System.out.println("소수의 합 :" + sum);
+	}
+}
